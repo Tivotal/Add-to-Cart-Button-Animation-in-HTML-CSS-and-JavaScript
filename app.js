bfe7998fa196682e2fe955766492803e6da9ba92
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-08-29 08:21:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+/* Created by Tivotal */
+
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!btn.classList.contains("adding")) {
+    btn.classList.add("adding");
+
+    setTimeout(() => {
+      btn.classList.remove("adding");
+    }, 3600);
+  }
+});
